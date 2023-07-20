205b4eb0bb926d1664a0024554e2590f4dee22e4
Hiba-chokri <111317168+Hiba-chokri@users.noreply.github.com>
2023-07-20 11:56:30 +0100

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,30 @@
+def main():
+    print("This program converts US to dollars to Pounds sterling ")
+    print()
+
+    dollars = eval(input("Enter amount in dollars: "))
+
+    pounds = convert_to_pounds(dollars)
+
+    print("That is",pounds,"pounds.")
+
+
+convert_to_pounds = lambda dollars: dollars * 0.82
+
+main()
+
+def main():
+    print("This program converts US to dollars to Pounds sterling ")
+    print()
+
+    dollars = eval(input("Enter amount in dollars: "))
+
+    pounds = converst_to_pounds(dollars)
+
+    print("That is",pounds,"pounds.")
+
+
+convert_to_pounds = lambda dollars: dollars * 0.82
+
+main()
+
