bcc3b3bff01e672d4f8afb2260677bae80168018
Hiba-chokri <111317168+Hiba-chokri@users.noreply.github.com>
2023-07-20 11:57:59 +0100

    Update convert.py

--- convert.py
+++ convert.py
@@ -13,18 +13,3 @@ convert_to_pounds = lambda dollars: dollars * 0.82
 
 main()
 
-def main():
-    print("This program converts US to dollars to Pounds sterling ")
-    print()
-
-    dollars = eval(input("Enter amount in dollars: "))
-
-    pounds = converst_to_pounds(dollars)
-
-    print("That is",pounds,"pounds.")
-
-
-convert_to_pounds = lambda dollars: dollars * 0.82
-
-main()
-
